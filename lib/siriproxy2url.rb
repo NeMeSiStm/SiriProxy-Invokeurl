da2f8ce3048f7409f1388312921bb144ff81ec46
NeMeSiS_tm <nemesistm@gmail.com>
2013-02-18 05:34:49 +0100

    changed äö with ae oe for testing purposes

--- lib/siriproxy2url.rb
+++ lib/siriproxy2url.rb
@@ -11,19 +11,19 @@ class SiriProxy::Plugin::2url < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
   end
 
-  listen_for /sonos erhöhe Lautstärke im Badezimmer/i do
+  listen_for /sonos erhoehe Lautstaerke im Badezimmer/i do
     open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeUp")
       
-    say "Die Lautstärke im Badezimmer wurde erhöht."
+    say "Die Lautstaerke im Badezimmer wurde erhoeht."
       
     request_completed
     #Die Lautstärke wird um einen Punkt erhöht.
   end
 
-  listen_for /sonos veringere Lautstärke im Badezimmer/i do
+  listen_for /sonos veringere Lautstaerke im Badezimmer/i do
     open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeDown")
     
-    say "Die Lautstärke im Badezimmer wurde verringert."
+    say "Die Lautstaerke im Badezimmer wurde verringert."
     
     request_completed
     #Die Lautstärke wird um einen Punkt verringert.
@@ -38,7 +38,7 @@ class SiriProxy::Plugin::2url < SiriProxy::Plugin
     #Musik im Badezimmer Ein.
   end
 
-  listen_for /test sonos/i do
+  listen_for /sonos/i do
     say "Hallo Michael, Sonos zu PHP funktioniert einwandfrei. Glaube ich zumindest." #say something to the user!
 
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
