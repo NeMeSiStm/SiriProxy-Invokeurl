2327b0e8d86844de2e54d1868ef35e335c5b3b42
NeMeSiS_tm <nemesistm@gmail.com>
2013-02-18 04:35:13 +0100

    trying to get it running

--- lib/siriproxy2url.rb
+++ lib/siriproxy2url.rb
@@ -11,32 +11,6 @@ class SiriProxy::Plugin::2url < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
   end
 
-  listen_for /sonos erhöhe Lautstärke im Badezimmer/i do
-    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeUp")
-      
-    say "Die Lautstärke im Badezimmer wurde erhöht."
-      
-    request_completed
-    #Die Lautstärke wird um einen Punkt erhöht.
-  end
-
-  listen_for /sonos veringere Lautstärke im Badezimmer/i do
-    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=VolumeDown")
-    
-    say "Die Lautstärke im Badezimmer wurde verringert."
-    
-    request_completed
-    #Die Lautstärke wird um einen Punkt verringert.
-  end
-
-  listen_for /sonos starte die Wiedergabe im Badezimmer/i do
-    open("http://milkyway.merkur.local/sonos/index.php?zone=Badezimmer&do=Play")
-    
-    say "Die Musik im Badezimmer wurde gestartet."
-    
-    request_completed
-    #Musik im Badezimmer Ein.
-  end
 
   listen_for /test sonos/i do
     say "Hallo Michael, Sonos zu PHP funktioniert einwandfrei. Glaube ich zumindest." #say something to the user!
